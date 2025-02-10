be0153fb747914a87f925b3643d1497452e66197
Alisson Correa Alves <aliba.truta@gmail.com>
2025-02-10 16:28:28 -0300

    substr php

---
+++ subtr.php
@@ -0,0 +1,9 @@
+<?php
+
+$frase = "Ola, Mundo!\n";
+
+$parte = substr($frase, 5, 5); //comeca no indice 5 e pega 5 caracteres
+echo $parte; //Saida: Mundo
+
+
+?>
--- uscfirstAndwords.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$texto = "php é incrível!\n";
-
-echo ucfirst($texto); // Saída: Php é incrível!
-echo ucwords($texto); // Saída: Php É Incrível!
-
-?>
