8d99e32308841cb7ebe1d868abb6949128cb2469
Alisson Correa Alves <aliba.truta@gmail.com>
2025-02-10 16:37:21 -0300

    str_replace

---
+++ str_replace.php
@@ -0,0 +1,9 @@
+<?php
+
+$frase = "Eu amo Java!\n";
+
+$novaFrase = str_replace("Java", "PHP", $frase);
+
+    echo $novaFrase; //Saida: Eu amo PHP!
+
+?>
--- subtr.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$frase = "Ola, Mundo!\n";
-
-$parte = substr($frase, 5, 5); //comeca no indice 5 e pega 5 caracteres
-echo $parte; //Saida: Mundo
-
-
-?>
