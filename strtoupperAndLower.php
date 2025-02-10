56fd38cfd5aa3899bc91790cc8332af3a2baa2cc
Alisson Correa Alves <aliba.truta@gmail.com>
2025-02-10 16:19:34 -0300

    uscfirst e ucwords

--- strtoupperAndLower.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$texto = "Ola, Mundo!\n";
-
-echo strtoupper($texto); //caixa alta
-echo strtolower($texto); //caixa baixa
-
-?>
---
+++ uscfirstAndwords.php
@@ -0,0 +1,8 @@
+<?php
+
+$texto = "php é incrível!\n";
+
+echo ucfirst($texto); // Saída: Php é incrível!
+echo ucwords($texto); // Saída: Php É Incrível!
+
+?>
