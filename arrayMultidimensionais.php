96546862a171121c58ead4ee7ecb2638402edead
Alisson Correa Alves <aliba.truta@gmail.com>
2025-02-13 19:16:31 -0300

    Array Multidimensionais

---
+++ arrayMultidimensionais.php
@@ -0,0 +1,11 @@
+<?php
+
+$alunos = [
+    ["nome" => "Ana", "nota" => 9],
+    ["nome" => "Bruno", "nota" => 7.5],
+    ["nome" => "Carla", "nota" => 8]
+];
+
+echo $alunos[0]["nome"]; //saida: Bruno
+
+?>
