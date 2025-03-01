ef9dd94a0bc576e2233b6080013e36eea13b54ec
Alisson Correa Alves <aliba.truta@gmail.com>
2025-03-01 18:37:01 -0300

    nota

---
+++ teste.php
@@ -0,0 +1,13 @@
+<?php
+
+$nota = 85;
+
+if ($nota >= 90) {
+    echo "Voce tirou um A. \n";
+} elseif ($nota >= 70) {
+    echo "Voce tirou um B.\n";
+} else {
+    echo "Voce tem que melhorar.\n";
+}
+
+?>
